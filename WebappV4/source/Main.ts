59ce62613d9c61f7790fcbb31c11392db0ef40f4
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2021-07-23 15:03:59 -0400

    Separate HTML into several documents

--- WebappV4/source/Main.ts
+++ WebappV4/source/Main.ts
@@ -4,7 +4,8 @@
  * @returns {GoogleAppsScript.HTML.HtmlOutput} The central webapp page
  */
 function doGet(event: Webapp.GogleAppsScript.EventType) {
-  return HtmlService.createHtmlOutputFromFile("html/index");
+  return HtmlService.createTemplateFromFile("html/Index").evaluate().setTitle("WebappV4")
+    .setFaviconUrl("https://storage.googleapis.com/eggonomics/resources/Egg%20Logo%20v2.png");
 }
 /**
  * Request some kind of server-side operation to be done.
@@ -12,4 +13,12 @@ function doGet(event: Webapp.GogleAppsScript.EventType) {
  */
 function doPost(event: Webapp.GogleAppsScript.EventType) {
 
+}
+/**
+ * Add an HTML element to a page. Note: requires client-side actions to load.
+ * @param {string} file The element to install
+ * @returns {string} The parsed element
+ */
+function installHtmlElement(file: string) {
+  return HtmlService.createTemplateFromFile(file).evaluate().getContent();
 }
