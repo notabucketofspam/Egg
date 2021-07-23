f2950dad0c93c1f70121154310308dc51fbb06f4
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2021-07-23 07:52:07 -0400

    Fix minor typo

--- WebappV4/postbuild.bat
+++ WebappV4/postbuild.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 FOR %%I IN (.) DO SET _currentDirectory=%%~nxI
-ROBOCOPY /IS build\\%_currentDirectory%\\source final > nul
+ROBOCOPY /IS build\\%_currentDirectory%\\source final > NUL
 EXIT 0
