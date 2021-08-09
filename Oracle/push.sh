f50a5fb5fd2752e02482bd6651d43ca5480895de
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2021-08-09 19:23:33 -0400

    Add options to push

--- Oracle/push.sh
+++ Oracle/push.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
-domain="eggonomics.net"
-npm run build --silent
-rsync --compress --recursive \
+_domain="eggonomics.net"
+_options="hlr"
+_print_help() {
+  echo "Push local project to remote"
+  echo "Options:"
+  echo "  -h    print this message"
+  echo "  -l    reload dependencies on remote"
+  echo "  -r    restart application on remote"
+}
+_push() {
+  npm run build --silent
+  rsync --compress --recursive \
 --include-from="rsync-include.txt" --exclude-from="rsync-exclude.txt" \
---delete -e "ssh -i \"./httpd-private-key\"" ./ httpd@$domain:/httpd/egg/
-ssh -i "./httpd-private-key" httpd@$domain "cd /httpd/egg && npm run restart --silent"
-echo && echo "Done"
+--delete -e "ssh -i \"./httpd-private-key\"" ./ httpd@$_domain:/httpd/egg/
+}
+while getopts "$_options" _flag; do
+  case "${_flag}" in
+    h)_print_help
+      exit;;
+  esac
+done
+OPTIND=1
+_push
+while getopts "$_options" _flag; do
+  case "${_flag}" in
+    l)ssh -i "./httpd-private-key" httpd@$_domain \
+"cd /httpd/egg && npm run reload-dependencies --silent";;
+    r)ssh -i "./httpd-private-key" httpd@$_domain \
+"cd /httpd/egg && npm run restart --silent"
+      echo;;
+  esac
+done
+echo "Done"
