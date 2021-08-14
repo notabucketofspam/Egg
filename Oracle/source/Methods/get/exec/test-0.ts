80874fa06c2c5d5ef4a397496102620771f7b3a7
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2021-08-14 13:11:59 -0400

    Add callback to print test results

---
+++ Oracle/source/Methods/get/exec/test-0.ts
@@ -0,0 +1,13 @@
+// Express setup
+import * as Express from "express";
+// Handler
+export const method = "get";
+export const route = "/exec/test-0";
+export async function exec(request: Express.Request, response: Express.Response) {
+  response.send(
+    `npm test: execute test command on server
+Synopsis: npm test [-- TEST_NO]
+Options:
+  TEST_NO    number of the test to run; default 0`
+  );
+}
