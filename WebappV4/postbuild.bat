455ce6ef61b551db65b83edfe67cfb518352e315
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2021-09-21 02:37:37 -0400

    Switch Bus3 to a direct dependency

--- WebappV4/postbuild.bat
+++ WebappV4/postbuild.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 FOR %%I IN (.) DO SET _currentDirectory=%%~nxI
-ROBOCOPY /IS build\\%_currentDirectory%\\source final > NUL
+ROBOCOPY /IS build\%_currentDirectory%\source final > NUL
+ROBOCOPY /IS build\Bus3\source final > NUL
 EXIT 0
