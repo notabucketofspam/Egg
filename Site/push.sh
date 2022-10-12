281ac477887204f180d75e3e6ef35f3dd54e1581
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-10-11 23:24:55 -0400

    Add push script

---
+++ Site/push.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+_domain="eggonomics.net"
+_options="h"
+_print_help() {
+  echo "Push compiled site to remote"
+  echo "Options:"
+  echo "  -h    print this message"
+}
+_push() {
+  ng build --progress=false
+  rsync --compress --recursive \
+--include-from="rsync-include.txt" --exclude-from="rsync-exclude.txt" \
+--delete -e "ssh -i \"./httpd-private-key\"" ./ httpd@$_domain:/httpd/egg/
+}
+while getopts "$_options" _flag; do
+  case "${_flag}" in
+    h)_print_help
+      exit;;
+  esac
+done
+OPTIND=1
+_push
+echo "Done"
