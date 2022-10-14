7b037e96461123c56d5eec2c9b5df2b61034426f
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-10-14 13:56:20 -0400

    Make some properties private

--- Oracle/source/ExtWSS.ts
+++ Oracle/source/ExtWSS.ts
@@ -1,13 +1,13 @@
 // WebSocket setup
 import { WebSocket, WebSocketServer, ServerOptions } from "ws";
-const pingFrame = Uint8Array.from([0x9]);
-const pongFrame = Uint8Array.from([0xA]);
 /**
  * Wrapper for WebSocketServer with browser-compatible ping-pong
  */
 export default class ExtWSS extends WebSocketServer {
-  aliveClients: Map<WebSocket, boolean>;
-  pingTimer: NodeJS.Timer;
+  private pingFrame = Uint8Array.from([0x9]);
+  private pongFrame = Uint8Array.from([0xA]);
+  private aliveClients: Map<WebSocket, boolean>;
+  private pingTimer: NodeJS.Timer;
   constructor(options?: ServerOptions, callback?: () => void) {
     super(options, callback);
     this.aliveClients = new Map();
@@ -19,14 +19,14 @@ export default class ExtWSS extends WebSocketServer {
           continue;
         }
         server.aliveClients.set(client, false);
-        client.send(pingFrame);
+        client.send(server.pingFrame);
       }
     }, 30000, this);
     const server = this;
     this.on("connection", function (client, request) {
       server.aliveClients.set(client, true);
       client.on("message", function (data, isBinary) {
-        if (isBinary && (data as Buffer).length === 1 && (data as Buffer)[0] === pongFrame[0]) {
+        if (isBinary && (data as Buffer).length === 1 && (data as Buffer)[0] === server.pongFrame[0]) {
           //console.log("Pong!");
           server.aliveClients.set(this, true);
         } else {
