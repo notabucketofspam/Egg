804ea3ddd2bc9f0773e5f3d359c8075fcc855ef5
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-10-19 17:14:34 -0400

    Relocate home page

--- Site/src/app/app.component.ts
+++ Site/src/app/app.component.ts
@@ -1,29 +1,8 @@
 import { Component } from '@angular/core';
-import { WebSocketService } from './websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(private websocket: WebSocketService) { }
-  title = 'Eggonomics';
-  private buttonToggled = false;
-  buttonText = "Plug it in, coach";
-  messages: string[] = [];
-  toggleWebSocket() {
-    this.buttonToggled = !this.buttonToggled;
-    if (this.buttonToggled) {
-      // Connect to WebSocket
-      this.websocket.subscribe();
-      this.buttonText = "Get it outta here";
-      this.messages.push("New connection.", "Check console (F12) for pings.");
-    } else {
-      // Disconnect from WebSocket
-      this.websocket.complete();
-      this.buttonText = "Plug it in, coach";
-      this.messages.push("Closed connection.");
-    }
-  }
-}
+export class AppComponent { }
--- Site/src/app/home/home.component.ts
+++ Site/src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { WebSocketService } from '../websocket.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HomeComponent {
+  constructor(private websocket: WebSocketService) { }
+  title = 'Eggonomics';
+  private buttonToggled = false;
+  buttonText = "Plug it in, coach";
+  messages: string[] = [];
+  game = "sauce";
+  toggleWebSocket() {
+    this.buttonToggled = !this.buttonToggled;
+    if (this.buttonToggled) {
+      // Connect to WebSocket
+      this.websocket.subscribe();
+      this.buttonText = "Get it outta here";
+      this.messages.push("New connection.", "Check console (F12) for pings.");
+    } else {
+      // Disconnect from WebSocket
+      this.websocket.complete();
+      this.buttonText = "Plug it in, coach";
+      this.messages.push("Closed connection.");
+    }
   }
-
 }
