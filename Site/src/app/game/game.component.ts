ce254ff5e703dffad3a655e1d0ed68d53f426c4e
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-10-19 17:09:32 -0400

    Sample game page

--- Site/src/app/game/game.component.ts
+++ Site/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-
-  constructor() { }
-
+  game = "";
+  constructor(private route: ActivatedRoute) { }
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.game = params['game'];
+    });
   }
-
 }
