ee7c9ea5ce20450911db20925d3aa3b5f1648cb4
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-10-19 20:39:58 -0400

    Set proper page title

--- Site/src/app/game/game.component.ts
+++ Site/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,10 +9,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GameComponent implements OnInit {
   game = "";
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private title: Title) { }
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.game = params['game'];
+      this.title.setTitle(`Game ${this.game} | Eggonomics`);
     });
   }
 }
