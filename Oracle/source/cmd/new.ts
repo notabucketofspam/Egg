392c93441cbff2c2c9bda68e674ba7ff6d9cb13a
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-10-30 14:17:03 -0400

    New game / add user

---
+++ Oracle/source/cmd/new.ts
@@ -0,0 +1,21 @@
+import { Util, RedisReply } from "../Util.js";
+// Command
+type New = {
+  cmd: "new",
+  user: string
+};
+export const cmd = "new";
+export async function exec({ client, activeGames, ioredis, scripts }: Util, data: New) {
+  const gameKey = Date.now().toString(16).padStart(14, "0");
+  const newStatus = await ioredis.evalsha(scripts["new"], 0, gameKey) as RedisReply;
+  if (newStatus === "OK") {
+    const addUserStatus = await ioredis.evalsha(scripts["add-user"], 0, gameKey, data.user) as RedisReply;
+    if (addUserStatus === "OK") {
+      client.send(JSON.stringify({ cmd: "new", newGame: gameKey }));
+    } else {
+      client.send(JSON.stringify({ cmd: "new", error: "Failed to add user" }));
+    }
+  } else {
+    client.send(JSON.stringify({ cmd: "new", error: "Failed to create new game" }));
+  }
+}
