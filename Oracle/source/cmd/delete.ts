840c1cdb2ba063aa88b857b80a0f749b667b3ece
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-03 13:36:52 -0400

    Disconnect clients on game delete

--- Oracle/source/cmd/delete.ts
+++ Oracle/source/cmd/delete.ts
@@ -1,4 +1,4 @@
-import { Util } from "../Util.js";
+import { RedisReply, Util } from "../Util.js";
 // Command
 type Delete = {
   cmd: "delete",
@@ -6,5 +6,17 @@ type Delete = {
 };
 export const cmd = "delete";
 export async function exec({ client, aliveClients, ioredis, scripts }: Util, data: Delete) {
-  const status = await ioredis.evalsha(scripts["delete"], 0, data.game);
+  const status = await ioredis.evalsha(scripts["delete"], 0, data.game) as RedisReply;
+  if (status === "OK") {
+    for (const [aliveClient, clientMeta] of aliveClients) {
+      if (clientMeta.game === data.game) {
+        aliveClient.off("message", () => void 0);
+        aliveClient.off("close", () => void 0);
+        aliveClient.terminate();
+        aliveClients.delete(aliveClient);
+      }
+    }
+  } else {
+    client.send(JSON.stringify({ cmd: "delete", err: "ENOGAME", why: "The game provided does not exist" }));
+  }
 }
