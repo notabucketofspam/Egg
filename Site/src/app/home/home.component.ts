4d4d8e4cf9259b846934dbd7c6e174269ef21896
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-07 18:41:50 -0500

    Remove WebSocket test button

--- Site/src/app/home/home.component.ts
+++ Site/src/app/home/home.component.ts
@@ -11,26 +11,8 @@ import { WebSocketService } from '../websocket.service';
 })
 export class HomeComponent implements OnDestroy {
   constructor(private websocket: WebSocketService) { }
-  private buttonToggled = false;
-  buttonText = "Plug it in, coach";
   messages: string[] = [];
   private subscriptions: Record<string, Subscription> = {};
-  toggleWebSocket() {
-    this.buttonToggled = !this.buttonToggled;
-    if (this.buttonToggled) {
-      // Connect to WebSocket
-      this.buttonText = "Get it outta here";
-      this.subscriptions["toggle"] = this.websocket.subscribe();
-      this.messages.push("New connection.");
-      if (!environment.production)
-        this.messages.push("Check console (F12) for pings.");
-    } else {
-      // Disconnect from WebSocket
-      this.subscriptions["toggle"].unsubscribe();
-      this.buttonText = "Plug it in, coach";
-      this.messages.push("Closed connection.");
-    }
-  }
   listGames() {
     this.subscriptions["ls"] = this.websocket.subscribe({
       next: (value) => {
@@ -43,8 +25,6 @@ export class HomeComponent implements OnDestroy {
     this.websocket.nextJ({ cmd: Cmd.Ls });
   }
   ngOnDestroy() {
-    if (this.subscriptions["toggle"])
-      this.subscriptions["toggle"].unsubscribe();
     if (this.subscriptions["ls"])
       this.subscriptions["ls"].unsubscribe();
   }
