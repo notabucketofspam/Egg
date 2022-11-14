c92c158e36b1ea3595a32f93852c6f554e2deee4
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-14 16:48:13 -0500

    Better display of stocks

--- Site/src/app/public-work/public-work.component.ts
+++ Site/src/app/public-work/public-work.component.ts
@@ -22,15 +22,15 @@ export class PublicWorkComponent implements OnInit, OnDestroy {
     "Dog Water"
   ];
   stockTable: string[][] = [
-    ["---", "---"],
-    ["Cathy:CREAM", "CREAM | Cathy's Creamed Corn"], ["Cathy:BEER", "BEER | Cathy's Cold Ones"],
-    ["Cathy:CRUNCH", "CRUNCH | Cathy Crunch"], ["Cathy:ROLL", "ROLL | Cathy's Kaiser Rolls"],
-    ["Terry:TOWER", "TOWER | Terry Towers"], ["Terry:TAP", "TAP | Terry's Taphouses"],
-    ["Terry:TIME", "TIME | Terry's Timeshares"], ["Terry:TENT", "TENT | Terry's Tents"],
-    ["Gary:CALC", "CALC | Gary's Graphing Solutions"], ["Gary:GUI", "GUI | Gary's GUIs"],
-    ["Gary:GLIT", "GLIT | Glitches with Gary"], ["Gary:GPU", "GPU | Gary's Graphical Solutions"],
-    ["Doug:CANN", "CANN | Doug's Dispenceries"], ["Doug:DOOD", "DOOD | Doodles by Doug"],
-    ["Doug:DUG", "DUG | Doug-Outs"], ["Doug:CLUB", "CLUB | Doug's Dance Club"]
+    ["---", "---", "---"],
+    ["Cathy:CREAM", "CREAM", "Cathy's Creamed Corn"], ["Cathy:BEER", "BEER", "Cathy's Cold Ones"],
+    ["Cathy:CRUNCH", "CRUNCH", "Cathy Crunch"], ["Cathy:ROLL", "ROLL", "Cathy's Kaiser Rolls"],
+    ["Terry:TOWER", "TOWER", "Terry Towers"], ["Terry:TAP", "TAP", "Terry's Taphouses"],
+    ["Terry:TIME", "TIME", "Terry's Timeshares"], ["Terry:TENT", "TENT", "Terry's Tents"],
+    ["Gary:CALC", "CALC", "Gary's Graphing Solutions"], ["Gary:GUI", "GUI", "Gary's GUIs"],
+    ["Gary:GLIT", "GLIT", "Glitches with Gary"], ["Gary:GPU", "GPU", "Gary's Graphical Solutions"],
+    ["Doug:CANN", "CANN", "Doug's Dispenceries"], ["Doug:DOOD", "DOOD", "Doodles by Doug"],
+    ["Doug:DUG", "DUG", "Doug-Outs"], ["Doug:CLUB", "CLUB", "Doug's Dance Club"]
   ];
   @Input() pw!: Record<string, number>;
   @Output() raiseEE = new EventEmitter<[string, number]>();
@@ -50,7 +50,8 @@ export class PublicWorkComponent implements OnInit, OnDestroy {
       this.raiseEE.emit([this.raiseForm.controls["stock"].value,
       this.flavors.indexOf(this.raiseForm.controls["flavor"].value)]);
       alert(`Congradulations! You have raised a ${this.raiseForm.controls["flavor"].value} \
-Public Work in the company ${this.raiseForm.controls["stock"].value}!`);
+public work in the company ${this.stockTable
+          .find(value => value[0] === this.raiseForm.controls["stock"].value)![2]}!`);
     }
     this.raiseForm.reset({ stock: "---", flavor: "---"});
   }
