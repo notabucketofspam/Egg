873453828f7afb951be5fd7f5eb849e39d7a38d2
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-15 00:28:10 -0500

    Trade offer scaffolding

--- Site/src/app/trade-offer/trade-offer.component.ts
+++ Site/src/app/trade-offer/trade-offer.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-trade-offer',
   templateUrl: './trade-offer.component.html',
   styleUrls: ['./trade-offer.component.css']
 })
-export class TradeOfferComponent implements OnInit {
-
+export class TradeOfferComponent implements OnChanges {
+  menuOpen = false;
+  menuButtonClass = "MenuClosed";
+  menuClass = "NoDisplay";
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnChanges() { }
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    this.menuButtonClass = this.menuOpen ? "MenuOpen" : "MenuClosed";
+    this.menuClass = this.menuOpen ? "Display" : "NoDisplay";
   }
-
 }
