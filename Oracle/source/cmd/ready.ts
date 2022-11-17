24d932043e55a21a08369f2e21669bedfe53927e
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-16 23:55:35 -0500

    Server-side ready command

---
+++ Oracle/source/cmd/ready.ts
@@ -0,0 +1,19 @@
+import { fromMapReply, fromScriptError, Util } from "../Util.js";
+// Command
+type Ready = {
+  cmd: "ready",
+  game: string,
+  user: string,
+  ready: boolean
+};
+export const cmd = "ready";
+export async function exec({ client, aliveClients, ioredis, scripts }: Util, data: Ready) {
+  if (data.ready)
+    await ioredis.sadd(`game:${data.game}:ready`, data.user);
+  else
+    await ioredis.srem(`game:${data.game}:ready`, data.user);
+  const ready = ioredis.smembers(`game:${data.game}:ready`);
+  for (const [aliveClient, clientMeta] of aliveClients)
+    if (clientMeta.game === data.game)
+      aliveClient.send(JSON.stringify({ cmd: "update", ready }));
+}
