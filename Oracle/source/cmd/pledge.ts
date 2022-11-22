30c64f841897ec53ff349f206c5b118f676beeb8
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-22 00:59:21 -0500

    Pledge command

---
+++ Oracle/source/cmd/pledge.ts
@@ -0,0 +1,21 @@
+import { fromScriptError, toScriptKeys, Util } from "../Util.js";
+// Command
+type Pledge = {
+  cmd: "pledge",
+  game: string,
+  user: string,
+  pledge: number
+};
+export const cmd = "pledge";
+export async function exec({ client, aliveClients, ioredis, scripts }: Util, data: Pledge) {
+  const fields: string[] = ["users", "pledge"];
+  const keys = toScriptKeys(data.game, fields);
+  try {
+    await ioredis.evalsha(scripts["pledge"], keys.length, ...keys, 0, data.game, data.user, data.pledge);
+    const pledge: Record<string, number> = {};
+    pledge[data.user] = data.pledge;
+    client.send(JSON.stringify({ cmd: "update", pledge }));
+  } catch (err) {
+    client.send(fromScriptError("pledge", err as Error));
+  }
+}
