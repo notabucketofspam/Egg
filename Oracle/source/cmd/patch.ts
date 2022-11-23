e82835b5a0a99af83364dc4ef9e8ac51e81ad99a
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-23 12:35:45 -0500

    Catch script errors in patch

--- Oracle/source/cmd/patch.ts
+++ Oracle/source/cmd/patch.ts
@@ -9,29 +9,33 @@ type Patch = {
 };
 export const cmd = "patch";
 export async function exec({ client, aliveClients, ioredis, scripts }: Util, data: Patch) {
-  try {
-    // Start patching based on the current version
-    for (let i = data.ver; i < patches.length; ++i) {
-      patches[i](ioredis, scripts, data.game, i, data.users);
+  // Start patching based on the current version
+  for (let i = data.ver; i < patches.length; ++i) {
+    if (await patches[i](ioredis, scripts, data.game, i, data.users)) {
+      client.send(fromScriptError("patch", new Error("EPATCH")));
+      break;
     }
-    for (const [aliveClient, clientMeta] of aliveClients) {
-      if (clientMeta.game === data.game) {
-        // Cheating a little bit here: using "add-user" to force the client to
-        // send a "load" command back to the server
-        aliveClient.send(JSON.stringify({ cmd: "add-user" }));
-      }
+  }
+  for (const [aliveClient, clientMeta] of aliveClients) {
+    if (clientMeta.game === data.game) {
+      // Cheating a little bit here: using "add-user" to force the client to
+      // send a "load" command back to the server
+      aliveClient.send(JSON.stringify({ cmd: "add-user" }));
     }
-  } catch (err) {
-    client.send(fromScriptError("patch", err as Error));
   }
 }
 const patches: ((ioredis: Redis, scripts: Record<string, string>,
-  game: string, ver: number, users: string[]) => Promise<void>)[] = [
+  game: string, ver: number, users: string[]) => Promise<boolean>)[] = [
     async (ioredis, scripts, game, ver, users) => {
-      // ver 0 -> 1
-      // add "last-own" to all users
+    // ver 0 -> 1
+    // add "last-own" to all users
+    try {
       const fields = ["ver", "user"];
       const keys = toScriptKeys(game, fields, users);
       await ioredis.evalsha(scripts["patch"], keys.length, ...keys, users.length, game, ver);
+      return false;
+    } catch (err) {
+      return true;
+    }
   }
 ];
