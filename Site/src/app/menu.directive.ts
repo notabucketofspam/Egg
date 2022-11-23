f93730b2fcace6648de9adca16942a1d21fc9ff7
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-22 21:57:54 -0500

    Implement Menu directive

--- Site/src/app/menu.directive.ts
+++ Site/src/app/menu.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appMenu]'
 })
 export class MenuDirective {
-
-  constructor() { }
-
+  open = false;
+  constructor(private element: ElementRef) { }
+  @HostListener("click")
+  onClick() {
+    const menuButton = (this.element.nativeElement as Element).getElementsByClassName("MenuButton")[0];
+    const menu = (this.element.nativeElement as Element).getElementsByClassName("Menu")[0];
+    if (this.open) {
+      menuButton.classList.replace("Open", "Closed");
+      menu.classList.replace("Display", "NoDisplay");
+    } else {
+      menuButton.classList.replace("Closed", "Open");
+      menu.classList.replace("NoDisplay", "Display");
+    }
+    this.open = !this.open;
+  }
 }
