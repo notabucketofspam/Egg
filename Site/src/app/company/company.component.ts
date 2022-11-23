9da424fa7ef24483da4c10536c5b4d7b817c1442
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-11-22 22:05:40 -0500

    Fix missing fields

--- Site/src/app/company/company.component.ts
+++ Site/src/app/company/company.component.ts
@@ -69,7 +69,8 @@ export class CompanyComponent implements OnInit, OnDestroy, OnChanges {
             this.withdraw.user[user] = {
               own: {},
               member: {},
-              offers: []
+              offers: [],
+              "last-own": {}
             };
             Object.keys(this.state.user[user].own).forEach((com) => {
               if (com === this.comShort)
@@ -80,7 +81,8 @@ export class CompanyComponent implements OnInit, OnDestroy, OnChanges {
             this.projected.user[user] = {
               own: {},
               member: {},
-              offers: []
+              offers: [],
+              "last-own": {}
             };
             Object.keys(this.state.user[user].own).forEach((com) => {
               if (com === this.comShort)
