03205eade64fe6bd04959b2d5a0f82f9242b8a05
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-12-01 14:27:37 -0500

    Fix typos

--- Oracle/source/cmd/ls.ts
+++ Oracle/source/cmd/ls.ts
@@ -5,13 +5,12 @@ type Ls = {
 };
 export const cmd = "ls";
 export async function exec({ client, aliveClients, ioredis, scripts }: Util, data: Ls) {
+  // Need to smembers "games", then push them to keys
   const keys: string[] = [];
   const gamesSet = await ioredis.smembers("games");
-  for (const game in gamesSet) {
+  for (const game of gamesSet) {
     keys.push(`game:${game}:users`);
   }
-  // Need to smembers "games", then .push them to keys
-  //const games = fromMapReply(await ioredis.evalsha(scripts["ls"], 0) as RedisReply[]);
   const partialJson = await ioredis.evalsha(scripts["ls"], keys.length, ...keys, ...gamesSet);
   client.send(partialJson);
 }
