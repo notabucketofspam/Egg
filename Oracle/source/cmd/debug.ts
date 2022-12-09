f741ca66b2deb7a59ec2f901e31c7d550827144b
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-12-08 21:32:20 -0500

    Full server-side debug command

--- Oracle/source/cmd/debug.ts
+++ Oracle/source/cmd/debug.ts
@@ -3,14 +3,25 @@ import { fromScriptError, toScriptKeys, Util } from "../Util.js";
 type Debug = {
   cmd: "debug",
   game: string,
-  key: string,
-  value: string,
-  sadd: number
+  field: string,
+  user?: string,
+  userField?: string,
+  prop: string,
+  value: string
 };
 export const cmd = "debug";
 export async function exec({ client, aliveClients, ioredis, scripts }: Util, data: Debug) {
   try {
-    await ioredis.evalsha(scripts["debug"], 1, data.key, 0, data.game, data.value, data.sadd);
+    const fields = data.user && data.userField ? [data.field, "users"] : [data.field];
+    const users = data.user && data.userField ? [data.user] : undefined;
+    const userFields = data.user && data.userField ? [data.userField] : undefined;
+    const keys = toScriptKeys(data.game, fields, users, userFields);
+    const userCount = Number(data.user && data.userField);
+    const partialJson = await ioredis.evalsha(scripts["debug"], keys.length, ...keys,
+      userCount, data.game, String(data.user), data.field, data.prop, data.value);
+    for (const [aliveClient, clientMeta] of aliveClients)
+      if (clientMeta.game === data.game)
+        aliveClient.send(partialJson);
   } catch (err) {
     client.send(fromScriptError("debug", err as Error));
   }
